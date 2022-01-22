b10713f77f38b8b6bd8466d5fb7e8501622e8c5f
Christ Henry Loha <cristloha2205@gmail.com>
2022-01-22 15:20:13 +0800

    update

---
+++ lib/ui/pages/detail_page.dart
@@ -0,0 +1,223 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/widgets/photo_item.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget backroundImage() {
+      return Container(
+        width: double.infinity,
+        height: 450,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              'assets/image_destination1.png',
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget customShadow() {
+      return Container(
+        width: double.infinity,
+        margin: const EdgeInsets.only(
+          top: 236,
+        ),
+        height: 214,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              kWhiteColor.withOpacity(0),
+              Colors.black.withOpacity(0.95),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      Widget emblem() {
+        return Container(
+          margin: const EdgeInsets.only(top: 30),
+          width: 108,
+          height: 24,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/icon_emblem.png',
+              ),
+            ),
+          ),
+        );
+      }
+
+      Widget title() {
+        Widget rating() {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/icon_star.png'),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 1),
+              Text(
+                '4.5',
+                style: whiteTextStyle.copyWith(fontWeight: medium),
+              ),
+            ],
+          );
+        }
+
+        return Container(
+          margin: const EdgeInsets.only(
+            top: 256,
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Lake Ciliwung',
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      'Tangerang',
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: light,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              rating(),
+            ],
+          ),
+        );
+      }
+
+      Widget description() {
+        Widget about() {
+          return Text(
+            'About',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          );
+        }
+
+        Widget desc() {
+          return Text(
+            'Berada di jalur jalan provinsi yang menghubungkan Denpasar\nSingaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+            style: blackTextStyle.copyWith(
+              height: 2,
+            ),
+          );
+        }
+
+        Widget titlePhotos() {
+          return Text(
+            'Photos',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          );
+        }
+
+        Widget photos() {
+          return Row(
+            children: const [
+              PhotoItem(imageUrl: 'assets/image_photo1.png'),
+              PhotoItem(imageUrl: 'assets/image_photo2.png'),
+              PhotoItem(imageUrl: 'assets/image_photo3.png'),
+            ],
+          );
+        }
+
+        Widget titleInterest() {
+          return Text(
+            'Interests',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          );
+        }
+
+        return Container(
+          width: double.infinity,
+          margin: const EdgeInsets.only(top: 30),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 30,
+          ),
+          decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(18),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              about(),
+              const SizedBox(height: 6),
+              desc(),
+              const SizedBox(height: 20),
+              titlePhotos(),
+              const SizedBox(height: 6),
+              photos(),
+              const SizedBox(height: 20),
+              titleInterest(),
+            ],
+          ),
+        );
+      }
+
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+        width: double.infinity,
+        child: Column(
+          children: [
+            emblem(),
+            title(),
+            description(),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            backroundImage(),
+            customShadow(),
+            content(),
+          ],
+        ),
+      ),
+    );
+  }
+}
