1a680eb1b717643cbf6a11de0cd6ec0bf57ac714
Christ Henry Loha <cristloha2205@gmail.com>
2022-01-28 15:53:33 +0800

    update

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,7 +1,11 @@
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/pages/settings_page.dart';
+import 'package:airplane/ui/pages/transaction_page.dart';
+import 'package:airplane/ui/pages/wallet_page.dart';
 import 'package:airplane/ui/widgets/custom_bottom.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'home_page.dart';
 
 class MainPage extends StatelessWidget {
@@ -9,8 +13,19 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget buildContent() {
-      return const HomePage();
+    Widget buildContent(int currentIndex) {
+      switch (currentIndex) {
+        case 0:
+          return const HomePage();
+        case 1:
+          return const TransactionPage();
+        case 2:
+          return const WalletPage();
+        case 3:
+          return const SettingPage();
+        default:
+          return const HomePage();
+      }
     }
 
     Widget customBottomNavigation() {
@@ -30,26 +45,41 @@ class MainPage extends StatelessWidget {
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const [
+            children: [
               CustomBottomNavigationItem(
-                  imageUrl: 'assets/icon_home.png', isSelected: true),
-              CustomBottomNavigationItem(imageUrl: 'assets/icon_booking.png'),
-              CustomBottomNavigationItem(imageUrl: 'assets/icon_card.png'),
-              CustomBottomNavigationItem(imageUrl: 'assets/icon_settings.png'),
+                index: 0,
+                imageUrl: 'assets/icon_home.png',
+              ),
+              CustomBottomNavigationItem(
+                index: 1,
+                imageUrl: 'assets/icon_booking.png',
+              ),
+              CustomBottomNavigationItem(
+                index: 2,
+                imageUrl: 'assets/icon_card.png',
+              ),
+              CustomBottomNavigationItem(
+                index: 3,
+                imageUrl: 'assets/icon_settings.png',
+              ),
             ],
           ),
         ),
       );
     }
 
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: Stack(
-        children: [
-          buildContent(),
-          customBottomNavigation(),
-        ],
-      ),
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          backgroundColor: kBackgroundColor,
+          body: Stack(
+            children: [
+              buildContent(currentIndex),
+              customBottomNavigation(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/ui/pages/settings_page.dart
+++ lib/ui/pages/settings_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({Key? key}) : super(key: key);
+class SettingPage extends StatelessWidget {
+  const SettingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
